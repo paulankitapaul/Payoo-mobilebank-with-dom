773e56532fecaea2db218041f6ef31397f6f1cc1
paulankitapaul <paulankita.ctg@gmail.com>
2024-11-20 11:14:36 +0600

    add money done,pin number check is done also update the added value with total ammount is also done

--- js/home.js
+++ js/home.js
@@ -3,6 +3,9 @@
 // step 02: get the added money
 // step 03:get the pin number
 
+// step 04 : get the total ammout and add this to addMoney
+// step 05: update the value with total ammount
+
 
 
 
@@ -15,6 +18,23 @@ document.getElementById('add-money-btn').addEventListener('click',function(event
     // step 03:get the pin number
     const pinNumber = document.getElementById('pin-number').value;
     console.log(pinNumber)
+    if(pinNumber==='1234'){
+        console.log('money is added');
+        //  step 04 if pin is correct then get the total ammount
+        const totalAmmount = document.getElementById('total-ammout').innerText;
+    console.log(totalAmmount);
+    // convert it in number
+    const addMoneyNumber = parseFloat(addMoney);
+    const totalAmmountNumber = parseFloat(totalAmmount)
+    // update the value with toal ammount
+    const updateValue =addMoneyNumber +totalAmmountNumber ;
+    console.log(updateValue);
+    document.getElementById('total-ammout').innerText = updateValue;
 
+    }
+    else{
+        console.log("oopssss!!!!!! try agin")
+    }
+    
     
 })
