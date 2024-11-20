9d73e8a7f7e47582c38236bfa53a463de4a523a4
paulankitapaul <paulankita.ctg@gmail.com>
2024-11-20 10:37:17 +0600

    particially done ,event hanlder added in btn

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
-console.log('money is added')
+// step 01 btn a eventlisterner add korte hbe
+
+// step 02: get the added money
+// step 03:get the pin number
+
+
+
+
+// step 01 added event-listerner into the button 
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    // get the added money
+    const addMoney = document.getElementById('added-money').value;
+    console.log(addMoney);
+    // step 03:get the pin number
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber)
+
+    
+})
